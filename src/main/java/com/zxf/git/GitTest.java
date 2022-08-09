22c2fe1e85f0f94c55e9cae8f7f493d3900922c6
zxf <495576762@qq.com>
2022-08-09 11:38:58 +0800

    push test

--- src/main/java/com/zxf/git/GitTest.java
+++ src/main/java/com/zxf/git/GitTest.java
@@ -2,7 +2,8 @@ package com.zxf.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hahhaha");
+        System.out.println("hahhaha0");
+        System.out.println("hahhaha1");
         System.out.println("hahhaha2");
     }
 }
